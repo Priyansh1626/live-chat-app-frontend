417b6f15f9ca7f8cd46745b549931bc7b951f75f
priyansh pandey <priyanshpandey2055@gmail.com>
2023-08-23 07:09:13 +0530

    ready to go

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3999"
+    baseURL: "https://friendly-chat-backend.up.railway.app"
 })
 
 export default instance;
